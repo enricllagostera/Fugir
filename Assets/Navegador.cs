40a68d42e16b1ae8409452ba50f967499673f26f
enricllagostera <enricllagostera@gmail.com>
2014-08-23 15:55:54 -0300

    sistema de pathfinding basico, com grafo dinamico

---
+++ Assets/Navegador.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navegador : MonoBehaviour {
+
+	public float intervalo;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("AtualizarGrafo");
+	}
+	
+	// Update is called once per frame
+	IEnumerator AtualizarGrafo () {
+		while (enabled) {
+			AstarPath.active.UpdateGraphs(new Bounds(Vector3.zero, new Vector3(50, 50, 0)));
+			yield return new WaitForSeconds (intervalo);
+		}
+	}
+}
